157bf328d24fd37afdf619855ccb030bf205cf41
黄云 <huangyun34@163.com>
2021-02-22 11:22:37 +0800

    first

---
+++ my-jvm/src/main/java/com/my/jvm/App.java
@@ -0,0 +1,13 @@
+package com.my.jvm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
